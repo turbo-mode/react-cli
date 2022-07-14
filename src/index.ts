a9715cc4e5ed66bd460112f7ef98439c3379d791
Rubi Chandraputra <rubichandrap@gmail.com>
2022-07-15 01:24:02 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 
-// import { program } from 'commander';
-// import { generateComponentCommand } from 'src/commands';
-// import pkg from '../package.json';
-// import { getCliConfigFile } from './utils';
+import { program } from 'commander';
+import { generateComponentCommand } from 'src/commands';
+import pkg from '../package.json';
+import { getCliConfigFile } from './utils';
 
-// const exec = async (argv: string[]) => {
-//   const cliConfigFile = await getCliConfigFile();
+const exec = async (argv: string[]) => {
+  const cliConfigFile = await getCliConfigFile();
 
-//   generateComponentCommand(argv, cliConfigFile, program);
+  generateComponentCommand(argv, cliConfigFile, program);
 
-//   program.version(pkg.version);
-//   program.parse(argv);
-// };
+  program.version(pkg.version);
+  program.parse(argv);
+};
 
-// exec(process.argv);
-
-console.log('hello world');
+exec(process.argv);
