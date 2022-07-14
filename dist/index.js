e5738261ed6c6617d2f59735d5fa7b1b10e6b744
Rubi Chandraputra <rubichandrap@gmail.com>
2022-07-15 01:35:18 +0700

    test

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
-
+"use strict";
 // import { program } from 'commander';
 // import { generateComponentCommand } from 'src/commands';
 // import pkg from '../package.json';
 // import { getCliConfigFile } from './utils';
-
 // const exec = async (argv: string[]) => {
 //   const cliConfigFile = await getCliConfigFile();
-
 //   generateComponentCommand(argv, cliConfigFile, program);
-
 //   program.version(pkg.version);
 //   program.parse(argv);
 // };
-
 console.log('hello world');
