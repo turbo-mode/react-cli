3afbe692c338f46a55669b8f2c537abeba505fc3
Rubi Chandraputra <rubichandrap@gmail.com>
2022-07-15 09:35:14 +0700

    test

--- dist/src/templates/components/componentIndexTemplate.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var componentIndexTemplate = "\n  export { default } from './TemplateName'\n";
-exports.default = componentIndexTemplate;
