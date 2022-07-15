8f7a3233762f6a387b61126e7217cd08837ff1ee
Rubi Chandraputra <rubichandrap@gmail.com>
2022-07-15 09:39:03 +0700

    test

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-import cli from '../dist/src/cli';
+// import cli from '../dist/src/cli';
 
-cli(process.argv);
+// cli(process.argv);
+
+import hello from './hello';
+
+hello();
